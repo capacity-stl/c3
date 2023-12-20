d58668dc2cd40f77f2f9d92539d989d6dc8606c3
Chris Elias <chris.elias@capacity.com>
2023-12-20 12:42:15 -0500

    KBP-105: Moving files around

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import styles from "./styles.module.scss";
+import styles from "./Button.module.scss";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const { className, ...restProps } = props;
