a394ebd7fa54f54cc6b1e754e55b1d38ee1c7441
Martin Sacco <martin.sacco@capacity.com>
2025-03-26 17:11:48 -0300

    Fix Badge unit tests

--- lib/components/Badge/Badge.stories.tsx
+++ lib/components/Badge/Badge.stories.tsx
@@ -209,8 +209,8 @@ export const RichContent: Story = {
       const boldText = within(badge).getByText('Role')
       const italicText = within(badge).getByText('Developer')
 
-      await expect(boldText).toHaveClass('font-bold')
-      await expect(italicText).toHaveClass('font-normal', 'italic')
+      await expect(boldText).toHaveClass('font-semibold')
+      await expect(italicText).toHaveClass('leading-3', 'uppercase')
     })
   },
 }
