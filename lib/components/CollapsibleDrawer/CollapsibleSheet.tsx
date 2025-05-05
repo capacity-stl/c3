ff1f81d370316413251e157e7538b22f30670c4d
Dan Larson <dan.larson@capacity.com>
2025-05-05 09:57:06 -0500

    KBP-657: Collapsible Drawer component

---
+++ lib/components/CollapsibleDrawer/CollapsibleSheet.tsx
@@ -0,0 +1,9 @@
+import { CollapsibleSheetProps } from './CollapsibleSheet.props'
+
+const CollapsibleSheet = (props: CollapsibleSheetProps) => {
+  const { children } = props
+
+  return <>{children}</>
+}
+
+export { CollapsibleSheet }
