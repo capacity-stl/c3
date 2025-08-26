a7fa0fbf9507cb2126c97c405e380d94bc41346b
Chris Elias <chris.elias@capacity.com>
2025-08-26 14:53:44 -0400

    CORE-2259: Added props - widths and template cols and rows

--- lib/components/Grid/Grid.tsx
+++ lib/components/Grid/Grid.tsx
@@ -3,10 +3,19 @@ import { cn } from '../../utils/cn'
 import { GridProps, gridVariants } from './Grid.props'
 
 const Grid = React.forwardRef<HTMLDivElement, GridProps>(
-  ({ className, children, testId, ...gridProps }, ref) => {
+  (
+    { className, children, testId, templateCols, templateRows, ...gridProps },
+    ref,
+  ) => {
     const mergedProps = {
       ...gridProps,
       testId: testId ?? 'grid-component',
+    }
+
+    const style: React.CSSProperties = {
+      ...(templateCols ? { gridTemplateColumns: templateCols } : {}),
+      ...(templateRows ? { gridTemplateRows: templateRows } : {}),
+      ...(gridProps.style || {}),
     }
 
     return (
@@ -14,6 +23,7 @@ const Grid = React.forwardRef<HTMLDivElement, GridProps>(
         ref={ref}
         className={cn(gridVariants({ ...mergedProps }), className)}
         data-testid={mergedProps.testId}
+        style={style}
       >
         {children}
       </div>
