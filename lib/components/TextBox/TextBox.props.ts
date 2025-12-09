17e0188dcd428a0b139fa93a4d621bb919343bbc
Chris Elias <chris.elias@capacity.com>
2025-12-09 10:34:13 -0500

    CORE-2585: Fixed build errors

--- lib/components/TextBox/TextBox.props.ts
+++ lib/components/TextBox/TextBox.props.ts
@@ -1,4 +1,4 @@
-import { cva, type VariantProps } from 'class-variance-authority'
+import { cva } from 'class-variance-authority'
 
 import { marginProps, type MarginPropTypes } from '@props/margin.props'
 import { paddingProps } from '@props/padding.props'
